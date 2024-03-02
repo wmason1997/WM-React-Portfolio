0e279c1f1515e9e94dbd0a184be96bc2ce030cbe
wmason1997 <williamcmason1997@gmail.com>
2024-03-02 15:42:34 -0800

    Centered div and added placeholder intake information

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,8 +2,31 @@
 
 const ContactPage = () => {
     return (
-        <div>
+        <div style={{display: "flex", justifyContent: "center"}}>
             {/* Your contact page content here */}
+            <form className="new-contact-submission">
+                <input
+                    type="text"
+                    placeholder = "Your name"
+                    name = "your_name"
+                ></input>
+                <br></br>
+                <input
+                    type="text"
+                    placeholder = "Your email"
+                    name = "your_name"
+                ></input>
+                <br></br>
+                <input
+                    type="text"
+                    placeholder = "Your message"
+                    name = "your_name"
+                ></input>
+                <br></br>      
+                <button className="contact-inquiry-button">Submit correspondence</button>          
+
+
+            </form>
         </div>
     );
 };
