269cf0f20f76e6fcd1b24807c3667a9ba5d4d33a
wmason1997 <williamcmason1997@gmail.com>
2024-03-02 16:17:45 -0800

    Got resume working

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,34 +1,24 @@
-
-
 const ContactPage = () => {
-    return (
-        <div style={{display: "flex", justifyContent: "center"}}>
-            {/* Your contact page content here */}
-            <form className="new-contact-submission">
-                <input
-                    type="text"
-                    placeholder = "Your name"
-                    name = "your_name"
-                ></input>
-                <br></br>
-                <input
-                    type="text"
-                    placeholder = "Your email"
-                    name = "your_name"
-                ></input>
-                <br></br>
-                <input
-                    type="text"
-                    placeholder = "Your message"
-                    name = "your_name"
-                ></input>
-                <br></br>      
-                <button className="contact-inquiry-button">Submit correspondence</button>          
-
-
-            </form>
-        </div>
-    );
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      {/* Your contact page content here */}
+      <form className="new-contact-submission">
+        <input type="text" placeholder="Your name" name="your_name"></input>
+        <br></br>
+        <input type="text" placeholder="Your email" name="your_email"></input>
+        <br></br>
+        <textarea
+          placeholder="Your message"
+          name="your_message"
+          style={{ height: "150px", width: "189px" }}
+        ></textarea>
+        <br></br>
+        <button className="contact-inquiry-button">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default ContactPage;
--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -4,7 +4,9 @@ const ResumePage = () => {
             {/* Your resume page content here */}
             <h1>Resume</h1>
                 <p>Click the link below to download my resume:</p>
-                <a href="../../assets/William_Mason_Resume.pdf" download="William_Mason_Resume.pdf">Download PDF</a>
+                <a href="http://localhost:3000/src/assets/William_Mason_Resume.pdf" download="http://localhost:3000/src/assets/William_Mason_Resume.pdf" >Download PDF</a> 
+                {/* Will have to change it later to the address based on the deployed site's URL */}
+            
                 <br></br>
             <div>
                 <p>Frontend Proficiencies</p>
