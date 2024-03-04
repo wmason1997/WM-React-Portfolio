b3468aa2611fb88c08e5270b4f818b0d71205b01
wmason1997 <williamcmason1997@gmail.com>
2024-03-03 22:36:02 -0800

    Added some About Me content.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,18 +1,16 @@
 //import React from 'react';
 
+// import stylings
+import './AboutPage.css';
+
+// import picture of myself
+import profilepicture from "../assets/William_Mason_Iceland_Waterfall_Pic.jpg";
+
 // This is a static page mocking an "About Us" section for our fake user data
 export default function AboutPage() {
     return (
       <div className="container pt-4">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-          impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-          architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-          adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-          mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-          nostrum temporibus ad omnis nam rerum eligendi.
-        </p>
+
         <section className="features-icons bg-light text-center m-4">
           <div className="container">
             <div className="row p-2">
@@ -21,10 +19,6 @@ export default function AboutPage() {
                   <div className="features-icons-icon d-flex">
                     <i className="bi-window m-auto text-primary" />
                   </div>
-                  <h3>Fully Responsive</h3>
-                  <p className="lead mb-0">
-                    This theme will look great on any device, no matter the size!
-                  </p>
                 </div>
               </div>
               <div className="col-lg-4">
@@ -32,9 +26,10 @@ export default function AboutPage() {
                   <div className="features-icons-icon d-flex">
                     <i className="bi-layers m-auto text-primary" />
                   </div>
-                  <h3>Bootstrap 5 Ready</h3>
+                  <h3>Welcome to my page!</h3>
+                  <img src={profilepicture} className="profile-image" alt='Picture of William Mason standing in front of Gulfoss Falls in Iceland'></img>
                   <p className="lead mb-0">
-                    Featuring the latest build of the new Bootstrap 5 framework!
+                  My name is William Mason.
                   </p>
                 </div>
               </div>
@@ -43,24 +38,15 @@ export default function AboutPage() {
                   <div className="features-icons-icon d-flex">
                     <i className="bi-terminal m-auto text-primary" />
                   </div>
-                  <h3>Easy to Use</h3>
-                  <p className="lead mb-0">
-                    Ready to use with your own content, or customize the source
-                    files!
-                  </p>
+
+
                 </div>
               </div>
             </div>
           </div>
         </section>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          tenetur maiores, dolor iusto dolorum ullam, natus deleniti blanditiis
-          impedit suscipit sed magnam alias in, repellat expedita hic explicabo
-          architecto soluta. About us Lorem ipsum, dolor sit amet consectetur
-          adipisicing elit. Velit voluptate exercitationem quaerat pariatur
-          mollitia, excepturi, voluptatem eveniet a dolor nobis ex veniam totam
-          nostrum temporibus ad omnis nam rerum eligendi.
+        I am a full stack web developer with a robust foundation in autonomous vehicles software, applied mathematics, and statistics. I am excited to fall down the rabbit hole that is software engineering and web development. In my free time, I can be found training for my next marathon or half-Ironman. I am also a self-proclaimed cinephile and card-carrying member of the AMC Stubs A-List, but my Letterboxd is not up to date, so my Strava will have to do below for now. Happy coding and thanks for stoping by!
         </p>
       </div>
     );
