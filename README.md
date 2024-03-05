e5eab0ff01e13d0f314ec19283d17d5ae7506094
wmason1997 <williamcmason1997@gmail.com>
2024-03-04 22:38:10 -0800

    Beginning to add the useState functionality on the ContactPage.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 ## Credits
 I had a tutoring session with Jacob Carver on 03/02/2024. Jacob helped me to get the Project component working within my Portfolio page as well as help me to set up my resume as downloadable. Thank you, Jacob!
+
+XLA for useState consultation for the Contact page error handling.
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,34 @@
+// import React, useState for error handling
+import React, { useState } from "react";
+
 const ContactPage = () => {
+
+  const [email, setEmail] = useState('');
+
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  }
+
+  const handleEmailChange = (event) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+
+    if (!validateEmail(newEmail)) {
+        // Display a notification to the user that the email address is invalid
+        console.log("Invalid email address entered."); /// CHANGE THIS TO DISPLAY TEXT ON PAGE
+    }
+}
+
+// DO SIMILAR THINGS FOR NAME AND MESSAGE BEING LEFT BLANK
+
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
       {/* Your contact page content here */}
       <form className="new-contact-submission">
         <input type="text" placeholder="Your name" name="your_name" required></input>
         <br></br>
-        <input type="text" placeholder="Your email" name="your_email" required></input>
+        <input type="email" value={email} name="your_email" onChange={handleEmailChange} required></input>
         <br></br>
         <textarea
           placeholder="Your message"
