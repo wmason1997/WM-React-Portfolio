4d30026dd65907917ebf2ef3f23ad86af712fffe
wmason1997 <williamcmason1997@gmail.com>
2024-03-04 18:30:42 -0800

    I added required, but need to add the useState state validation for error messages still.

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -3,14 +3,15 @@ const ContactPage = () => {
     <div style={{ display: "flex", justifyContent: "center" }}>
       {/* Your contact page content here */}
       <form className="new-contact-submission">
-        <input type="text" placeholder="Your name" name="your_name"></input>
+        <input type="text" placeholder="Your name" name="your_name" required></input>
         <br></br>
-        <input type="text" placeholder="Your email" name="your_email"></input>
+        <input type="text" placeholder="Your email" name="your_email" required></input>
         <br></br>
         <textarea
           placeholder="Your message"
           name="your_message"
-          style={{ height: "150px", width: "189px" }}
+          style={{ height: "150px", width: "189px" }} 
+          required
         ></textarea>
         <br></br>
         <button className="contact-inquiry-button">
